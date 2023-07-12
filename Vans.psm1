c21fe9e38b5ea193c2010d02becaf320e8d9e9fd
Nguyễn Văn Vận <vegetaz@outlook.com>
2023-07-12 13:37:42 +0700

    Get-URL

--- Vans.psm1
+++ Vans.psm1
@@ -1,1 +1,14 @@
 using namespace System.Collections.Generic
+
+function Get-URL {
+    param (
+        $url
+    )
+    try {
+        $driver.Navigate().GoToUrl($url)
+        Write-Host "Opening the $url website" -Verbose
+    }
+    catch [OpenQA.Selenium.WebDriverException] {
+        $driver.Navigate().Refresh()
+    }
+}
