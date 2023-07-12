6092943963714ae545f454c06918cecadeaecb10
Nguyễn Văn Vận <vegetaz@outlook.com>
2023-07-12 14:11:07 +0700

    Stop-WebDriver

--- Vans.psm1
+++ Vans.psm1
@@ -1,5 +1,17 @@
 using namespace System.Collections.Generic
 
+function Set-ImplicitWait {
+    param(
+        [Parameter(Mandatory=$true)]
+        [System.Management.Automation.Internal.PSObject]$driver,
+
+        [Parameter(Mandatory=$true)]
+        [int]$timeoutSeconds
+    )
+
+    $driver.Manage().Timeouts().ImplicitWait = [System.TimeSpan]::FromSeconds($timeoutSeconds)
+}
+
 function Get-URL {
     param (
         $url
@@ -11,4 +23,40 @@ function Get-URL {
     catch [OpenQA.Selenium.WebDriverException] {
         $driver.Navigate().Refresh()
     }
+}
+
+function Enter-ElementIdByJavaScript {
+    param(
+        [Parameter(Mandatory=$true)]
+        [System.Management.Automation.Internal.PSObject]$driver,
+
+        [Parameter(Mandatory=$true)]
+        [System.Management.Automation.Internal.PSObject]$elementId
+    )
+
+    $script = "arguments[0].click();"
+    $driver.ExecuteScript($script, @($elementId))
+}
+
+function Enter-ElementXpathByJavaScript {
+    param(
+        [Parameter(Mandatory=$true)]
+        [System.Management.Automation.Internal.PSObject]$driver,
+
+        [Parameter(Mandatory=$true)]
+        [System.Management.Automation.Internal.PSObject]$elementXpath
+    )
+
+    $script = "arguments[0].click();"
+    $driver.ExecuteScript($script, @($elementXpath))
+}
+
+function Stop-WebDriver {
+    param(
+        [Parameter(Mandatory=$true)]
+        [System.Management.Automation.Internal.PSObject]$driver
+    )
+
+    $driver.Quit()
+    $driver.Dispose()
 }
