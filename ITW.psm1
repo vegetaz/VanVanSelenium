3da71a48747b895801cadf74a57d3c0c8910dc72
Nguyễn Văn Vận <vegetaz@outlook.com>
2023-07-12 18:31:48 +0700

    Enter-ITWGetScreenshot

--- ITW.psm1
+++ ITW.psm1
@@ -75,3 +75,92 @@ function Expand-FolderCategory {
         $driver.Navigate().Refresh()
     }
 }
+
+function Get-ScreenshotITWByFirefox {
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$locationPathSaveImage,
+        [Parameter(Mandatory = $true)]
+        [string]$examTarget
+    )
+
+    try {
+        Set-Location $locationPathSaveImage
+        $fileName = Join-Path (Get-Location).Path "$examTarget-Detail.png"
+        $screenshot = $driver.GetFullPageScreenshot()
+        $screenshotBase64 = $screenshot.AsBase64EncodedString
+        [System.IO.File]::WriteAllBytes($fileName, [System.Convert]::FromBase64String($screenshotBase64))
+        Write-Host "Successfully taken screenshot for $examTarget" -Verbose
+        Start-Sleep -Milliseconds 1000
+    }
+    catch [OpenQA.Selenium.WebDriverException] {
+        Write-Error -Message "$_.Exception.Message"
+    }
+}
+
+function Enter-ExamGetScreenshot {
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$examTarget,
+        [Parameter(Mandatory = $true)]
+        [string]$locationPathSaveImage
+    )
+    
+    $examTargetPrefix = "//a[contains(text(),"
+    $examTargetSuffix = ")]"
+    $examTargetXPath = $examTargetPrefix + '"' + $examTarget + '"' + $examTargetSuffix
+
+    try {
+        Enter-ElementXpathByJavaScript($examTargetXPath)
+        Start-Sleep -Milliseconds 1000
+        $driver.Navigate().Refresh()
+        Write-Host "Refreshing Web browser to bypass: Browsing context has been discarded"
+        Start-Sleep -Milliseconds 1000
+        Enter-ElementIdByJavaScript('testDetailTab')
+        Start-Sleep -Milliseconds 1000
+        Get-ScreenshotITWByFirefox -locationPathSaveImage $locationPathSaveImage -examTarget $examTarget
+        Start-Sleep -Milliseconds 1000
+        Enter-ElementID('header_folders')
+        Start-Sleep -Milliseconds 1000
+        if ($driver.FindElement([OpenQA.Selenium.By]::XPath('//iframe[@id="home_iframe"]'))) {
+            Switch-ToIframe('home_iframe')
+        }
+        Start-Sleep -Milliseconds 1000
+    }
+    catch [OpenQA.Selenium.WebDriverException] {
+        Write-Error -Message "$_.Exception.Message"
+    }
+}
+
+function Enter-ITWGetScreenshot {
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$itw,
+        [Parameter(Mandatory = $true)]
+        [string]$username,
+        [Parameter(Mandatory = $true)]
+        [SecureString]$securePassword,
+        [Parameter(Mandatory = $true)]
+        [string]$examFolderName,
+        [Parameter(Mandatory = $true)]
+        [array]$listExams,
+        [Parameter(Mandatory = $true)]
+        [string]$locationPathSaveImage
+    )
+    
+    Enter-ITW -itw $itw -username $username -securePassword $securePassword
+    Expand-FolderCategory -examFolderName $examFolderName
+    Switch-ToIframe('home_iframe')
+    Start-Sleep -Seconds 1
+    Enter-ElementXpath('//*[contains(text(),"Tests")]')
+    Start-Sleep -Seconds 5
+    foreach ($examTarget in $listExams) {
+        Enter-ExamGetScreenshot -examTarget $examTarget -locationPathSaveImage $locationPathSaveImage
+    }
+    Start-Sleep -Seconds 1
+    Write-Host "Take screenshot completed!" -ForegroundColor DarkGreen
+    Start-Sleep -Seconds 1
+    Stop-WebDriver
+    Stop-Process -Name "geckodriver" -Force -ErrorAction SilentlyContinue
+    Invoke-Item $locationPathSaveImage
+}
