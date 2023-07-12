3ed180dba31733853ba1ac5ee8d7e637baebebaa
Nguyễn Văn Vận <vegetaz@outlook.com>
2023-07-12 17:11:59 +0700

    Update

--- ITW.psm1
+++ ITW.psm1
@@ -1,5 +1,26 @@
 using namespace System.Collections.Generic
 
+function Enter-ITW {
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$itw,
+        [Parameter(Mandatory=$true)]
+        [string]$username,
+        [Parameter(Mandatory=$true)]
+        [SecureString]$securePassword
+    )
+
+    Get-URL($itw)
+    Set-ElementValueById -elementId 'uname' -value $username
+    $plainTextPassword = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($securePassword))
+    Start-Sleep -Seconds 1
+    Set-ElementValueById -elementId 'pass' -value $plainTextPassword
+    Enter-ElementIdByJavaScript -elementId 'btnlogin'
+    Start-Sleep -Seconds 3
+    Enter-ElementXpathByJavaScript('//*[@id="header_headerlinksContent"]/button')
+    Start-Sleep -Seconds 3
+}
+
 function Expand-FolderCategory {
     param (
         [Parameter(Mandatory=$true)]
@@ -24,25 +45,4 @@ function Expand-FolderCategory {
         Write-Error -Message "$_.Exception.Message"
         $driver.Navigate().Refresh()
     }
-}
-
-function Enter-ITW {
-    param (
-        [Parameter(Mandatory=$true)]
-        [string]$itw,
-        [Parameter(Mandatory=$true)]
-        [string]$username,
-        [Parameter(Mandatory=$true)]
-        [SecureString]$securePassword
-    )
-
-    Get-URL($itw)
-    Set-ElementValueById -elementId 'uname' -value $username
-    $plainTextPassword = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($securePassword))
-    Start-Sleep -Seconds 1
-    Set-ElementValueById -elementId 'pass' -value $plainTextPassword
-    Enter-ElementIdByJavaScript -elementId 'btnlogin'
-    Start-Sleep -Seconds 3
-    Enter-ElementXpathByJavaScript('//*[@id="header_headerlinksContent"]/button')
-    Start-Sleep -Seconds 3
 }
